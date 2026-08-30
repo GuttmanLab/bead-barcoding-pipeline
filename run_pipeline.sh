24faebb5e4231bfc628b079d3f280cc76d5cb279
igoronzy <30717081+igoronzy@users.noreply.github.com>
2026-08-30 16:38:47 -0400

    Add files via upload

---
+++ run_pipeline.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Prevent locally installed user site-packages from superseding conda environments
+# - This environment variable will be propagated to jobs launched by snakemake
+# - Reference: https://github.com/conda/conda/issues/448
+export PYTHONNOUSERSITE=True
+
+# add --workflow-profile workflow/profiles/slurm for SLURM cluster job submission
+snakemake "$@" --workflow-profile workflow/profiles/slurm --configfile config/config.yaml
